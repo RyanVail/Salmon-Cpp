da931edd41759bee36dbea810d62f30bee21996a
RyanVail <111651152+RyanVail@users.noreply.github.com>
2022-09-20 07:29:13 -0400

    Embedded loops

--- examples/bf.s
+++ examples/bf.s
@@ -1,9 +1,10 @@
 .global _start
 _start:
+  mov sp, #0
   ldr r2, =file
   mov r3, sp // BF stack pointer
   sub sp, #65536
-  mov r5, #0 // TEMP
+  mov r5, #1
   b reset_m_l
   
 main_loop:
@@ -15,9 +16,13 @@ main_loop:
   beq back
   cmp r0, #43
   addeq r1, #1
+  lsleq r1, #24
+  lsreq r1, #24
   beq reset_m_l
   cmp r0, #45
   subeq r1, #1
+  lsleq r1, #24
+  lsreq r1, #24
   beq reset_m_l
   cmp r0, #93
   beq closing
@@ -39,7 +44,7 @@ closing:
   lsr r1, #24
   cmp r1, #0
   beq reset_m_l
-  sub r4, r2, #1 // r4 is the temp BF stack pointer
+  sub r4, r2, #0 // r4 is the temp BF stack pointer
   b reset
   c_loop:
   	// sub r5, #1 // The counter
@@ -48,11 +53,12 @@ closing:
 	cmp r0, #91
 	bne reset
 	opening:
-		cmp r5, #0
-		subne r5, #1
-		moveq r5, #0 // Reset the counter
+		sub r5, #1
+		cmp r5, #1
+		//subne r5, #1
+		//moveq r5, #0 // Reset the counter
   		subeq r2, r4, #0 // Set the stack pointer to the correct location
-		b reset_m_l
+		beq reset_m_l
 	reset:
 		ldrb r0, [r4, #-1]! // Incrament r4 backwards and load the char into r6
 		b c_loop
@@ -70,6 +76,6 @@ _end:
 	// If we hit a ']' go back and each time we go over a '[' add one and ']' sub one if we hit a '[' and we are at zero set the pointer there
 // + 43
 // - 45
-file: 
-	.string "[+>]++++"
+file:
+	.string "[[+]>+]" // This resets mem
 	.byte 0
