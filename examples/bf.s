6ebe864803506c56b7f35c2b525a06ce29e92525
RyanVail <111651152+RyanVail@users.noreply.github.com>
2022-09-22 06:42:54 -0400

    Add files via upload

--- examples/bf.s
+++ examples/bf.s
@@ -1,28 +1,21 @@
 .global _start
 _start:
-  mov sp, #0
   ldr r2, =file
   mov r3, sp // BF stack pointer
   sub sp, #65536
-  mov r5, #1
+  mov r5, #0 // TEMP
   b reset_m_l
   
 main_loop:
-  cmp r0, #0
-  beq _end
   cmp r0, #62
   beq forward
   cmp r0, #60
   beq back
   cmp r0, #43
   addeq r1, #1
-  lsleq r1, #24
-  lsreq r1, #24
   beq reset_m_l
   cmp r0, #45
   subeq r1, #1
-  lsleq r1, #24
-  lsreq r1, #24
   beq reset_m_l
   cmp r0, #93
   beq closing
@@ -44,7 +37,7 @@ closing:
   lsr r1, #24
   cmp r1, #0
   beq reset_m_l
-  sub r4, r2, #0 // r4 is the temp BF stack pointer
+  sub r4, r2, #1 // r4 is the temp BF stack pointer
   b reset
   c_loop:
   	// sub r5, #1 // The counter
@@ -53,21 +46,15 @@ closing:
 	cmp r0, #91
 	bne reset
 	opening:
-		sub r5, #1
-		cmp r5, #1
-		//subne r5, #1
-		//moveq r5, #0 // Reset the counter
+		cmp r5, #0
+		subne r5, #1
+		moveq r5, #0 // Reset the counter
   		subeq r2, r4, #0 // Set the stack pointer to the correct location
-		beq reset_m_l
+		b reset_m_l
 	reset:
 		ldrb r0, [r4, #-1]! // Incrament r4 backwards and load the char into r6
 		b c_loop
-
-_end:
-  //mov r7, #1
-  //swi 0
-  b _end
-
+  
 .data
 // > 62
 // < 60
@@ -75,7 +62,5 @@ _end:
 // ] 93
 	// If we hit a ']' go back and each time we go over a '[' add one and ']' sub one if we hit a '[' and we are at zero set the pointer there
 // + 43
-// - 45
-file:
-	.string "[[+]>+]" // This resets mem
-	.byte 0
+// - 45 
+file: .string "[->]"
---
+++ src/oldasm.cpp
@@ -0,0 +1,107 @@
+/*
+	This file handles turning lines into ASM - Ryan Vail 2022 Sep. 15th
+*/
+
+struct asm_settings
+{
+	debug = false; // If the asm has debug things attachted
+	std::vector<int> useable = {0,1,2,3} // The R0-R3 registers that are useable
+	int main[] = [0,1] // The main two registers used of doing computations
+}
+
+// TODO: Find a better way of doing this
+struct asm_single_expression
+{
+	int computation; // If this node is a computation otherwise 0 and 1 is reserved for commas in function inputs and list creation
+	variable var; // The variable of this node
+	function function_call; // The function we are calling
+	int const_value; // The value of this node if it is constant
+}
+
+// This writes assembly for if statments
+void if_to_asm(global &globals, std::vector<std::string> &file, int location)
+{
+	// This should go from the "if" to the '{' and use the function expression_to_asm on it
+}
+
+// This puts together the assembly of some computation
+// TODO: This function can only do operations between u32 and char
+// TODO: It would be very hard to do multiline optimizations with this function find a better way of doing this
+void expression_to_asm(global &globals, std::vector<std::string> &file, int location)
+{
+	// This goes through and counts parentheses until it reaches a ';'
+	std::vector<std::pair<int,int>> paretheses_counter; // The location of the paretheses then the counter
+	int counter = 0;
+	for (int i=location; i < file.size() || file[i] == ";"; i++)
+	{
+		if (file[i] == "(")
+		{ 
+			counter++;
+			paretheses_counter.push_back(std::make_pair(i, counter));
+		}
+		if (file[i] == ")") { counter--; }
+		if (counter < 0) { throw std::runtime_error("Uneven amount of closing and opening parentheses."); }
+	}
+	
+	for (int i=0; i < paretheses_counter; i++)
+	{
+		// This goes through and finds the value with the greatest count
+		pair<int,int> best;
+		best.first = -1;
+		best.second = 65536; // TODO: find a better way of doing this
+		for (int x=0; x < paretheses_counter; x++)
+		{
+			if (paretheses_counter[x].second < best.second) { best = paretheses_counter[x]; }
+		}
+		if (best.first == -1) { throw std::runtime_error("Something went wrong in the function expression_to_asm in the file asm.cpp."); }
+		
+		// This evaluates what's between those parentheses first
+		location = best.first;
+		
+	// EX. With badly optimized ASM:
+		// u32 a = return_this_value(3 + 3) + 5;
+		// 3 + 3 -> return_this_value(6) -> 6 + 5
+			// mov r0, #3
+				// mov r1, #3 
+			// add r0, r1
+				// bl return_this_value
+				// mov r1, #5
+				// add r0, r1
+		
+		// This should unwrap the expression removing paretheses and put it in the correct order using asm_single_expression
+		// TODO: THIS SHOULD ALSO CHECK FOR BRACKETS AS WELL AS PARETHESES!
+		
+		// If the variable has a '(' right after it without an operation seperating the two were calling a function
+			 // If the variable isn't just letters we send an error
+		// If the variable has a '[' right after it we are getting a certain index of an array
+				// We should check if that array itself and the array index is valid ie. in scope and of the valid length
+	// If the block we are working on only have commas we look for a function to the left of the '(' if we don't find one we send an error
+	}
+	
+	/*
+	bool had_last_value = false;
+	int last_value = 0;
+	for (int i=location; i < file.size(); i++)
+	{
+		if (!is_str_num(file[i]))
+		{
+			if (file[i] == "]") { return last_value; }
+			if (file[i] == "(" || file[i] == ")") { throw std::runtime_error("parentheses not yet supported sorry. TODO: add parenthese support. There, now it's your problem."); }
+			else if (check_expression(file[i], file[i+1]) == -1) { throw std::runtime_error("Unknow expression."); }
+			else
+			{
+				if (!had_last_value) { throw std::runtime_error("Expected const before expression."); }
+				last_value = compute_expression(last_value, check_expression(file[i], file[i+1]), get_str_num(file[i+1+(check_expression(file[i], file[i+1]) > 6)]));
+				i += 1 + (check_expression(file[i], file[i+1]) > 6);
+			}
+		}
+		else
+		{
+			if (had_last_value) { throw std::runtime_error("Expected an expression."); }
+			had_last_value = true;
+			last_value = get_str_num(file[i]);
+		}
+	}
+	return last_value;
+	*/
+}
