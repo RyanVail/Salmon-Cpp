18e1ec7e8597eaa61db3b158880bde0bc23661ad
RyanVail <111651152+RyanVail@users.noreply.github.com>
2022-09-30 13:16:56 -0400

    more RPN asm support.

--- src/asm.cpp
+++ src/asm.cpp
@@ -7,7 +7,8 @@
 // These holds single statments
 struct statment
 {
-	int type;
+	char type; // 0 -> constant; 1 -> variable; 2 -> function call; 3 -> add; 4 -> sub; 5 -> mul; 6 -> lsl; 7 -> lsr;
+	int value; // If this is a const its value; if variable the variable ID
 	std::vector<std::string> end_asm; // This is the asm that is added to the end when this is done
 };
 
@@ -35,11 +36,44 @@ void file_to_asm(std::vector<std::string> &file, &globals global)
 	* 42
 */
 
+// This takes stuff off the stack before doing an operation
+void off_load_stack(globals &global, std::vector<statment> &statments)
+{
+	// This makes sure we have enough things in the stack
+	if (!func && statments.size() < 2) { throw std::runtime_error("Erorr no more values in RPN stack"); }
+	
+	// This does this for both inputs
+	for (bool i = 0; i < 1; i++)
+	{
+		// If it's a const just move it into the register
+		if (statments[i].type == 0) { global.file_asm.push_back("mov r" + std::to_string(i) + " #" + std::to_string(statments[0].value)); }
+		if ()
+	}
+}
+
 // This turns a single expression into asm
-void evaluate_to_asm()
+void evaluate_to_asm(std::vector<std::string> &file, globals &global, int location)
 {
 	// This goes through and finds parentheses reordering the tokens and removing paretheses until it finds a ';', '{', or a '}'
 	std::vector<std::string> new_tokens;
 
-	// This doesn't check for anything being correct it just doesnt things if it's a + just add r0 and r1 but vars do have to be checked
+	// EX: 
+	//	3 5 + 3 , , test_func()
+	//	~~~~~~~~~~~~~~~~~~~~~
+	//	test_func(3 + 5, 3)
+	
+	std::vector<statment> token_stack;
+	
+	while (true) {  }
+	
+	// This goes through the tokens and outputs the assembly
+	for (std::vector<statment>::iterator statment_itr = token_stack.start(); statment != token_stack.end(); stack++)
+	{
+		statment current_statment = *statment_itr;
+		if (current_statment == 3 ) { global.file_asm.push_back("add r0, r1"); }
+		if (current_statment == 4 ) { global.file_asm.push_back("sub r0, r1"); }
+		if (current_statment == 5 ) { global.file_asm.push_back("mul r0, r1"); }
+		if (current_statment == 6 ) { global.file_asm.push_back("lsl r0, r1"); }
+		if (current_statment == 7 ) { global.file_asm.push_back("lsr r0, r1"); }
+	}
 }
